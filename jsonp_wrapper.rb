0499d4fcdc965d119bde306600b024f29fb17485
Nikita Vasilyev <me@elv1s.ru>
2010-02-02 00:36:59 +0300

    Multiple URLs. http://jsonpwrapper.com/?urls[]=http://example.com/&urls[]=http://elv1s.ru/x/pets.txt should work now.

--- jsonp_wrapper.rb
+++ jsonp_wrapper.rb
@@ -7,24 +7,49 @@ class JSONPWrapper
     @file_server = Rack::File.new(Dir.pwd << '/public')
   end
 
-  def call(env)
+  def call env
     request = Rack::Request.new env
-    callback = request.params['callback']
-    callback = 'grab_callback' if not callback or callback.empty?
-    if request.params.include? 'url'
-      headers = {'Content-Type' => 'application/javascript'}
-      url = URI.decode request.params['url']
-      url = "http://#{url}" if url !~ %r{^https?://}
-      begin
-        body = open(url).read.to_json or 'null'
-      rescue
-        body = 'null'
+    if request.params.include? 'urls' or request.params.include? 'url'
+      callback = request['callback']
+      callback = 'grab_callback' if not callback or callback.empty?
+      body = ''
+      if urls = request['urls']
+        body = fetch_many(urls).to_json
+      elsif url = request['url']
+        body = fetch(url).to_json
       end
-      response = %Q{#{callback}({"body":#{body}});\n}
-      [200, headers, response]
+      [
+        200,
+        {'Content-Type' => 'application/javascript'},
+        %Q{#{callback}({"body":#{body}});}
+      ]
     else
       env['PATH_INFO'] << 'index.html' if env['PATH_INFO'][-1, 1] == '/'
       @file_server.call env
+    end
+  end
+
+  def fetch url
+    raise ArgumentError if not url
+    url = "http://#{url}" if url !~ %r{^https?://}
+    open(url).read
+  rescue
+    nil
+  end
+
+  def fetch_many urls
+    if urls.size > 1
+      responses = []
+      threads = []
+      urls.each_with_index {|url, i|
+        threads << Thread.new {
+          responses[i] = fetch url
+        }
+      }
+      threads.each {|t| t.join}
+      responses
+    elsif urls.size == 1
+      urls.map {|u| fetch u}
     end
   end
 
--- test/jsonp_wrapper_test.rb
+++ test/jsonp_wrapper_test.rb
@@ -20,10 +20,36 @@ class JSONPWrapperTest < Test::Unit::TestCase
   def test_on_pets
     pets_url = 'http://elv1s.ru/x/pets.txt' # TODO: test it on localhost
     pets_json = open(pets_url).read.to_json
-    assert_equal %Q{feed({"body":#{pets_json}});\n}, get("/?url=#{pets_url}&callback=feed").body
-    assert_equal %Q{feed({"body":#{pets_json}});\n}, get("/?url=#{pets_url.sub('http://','')}&callback=feed").body
-    assert_equal %Q{yarrr_11({"body":#{pets_json}});\n}, get("/?url=#{pets_url}&callback=yarrr_11").body
-    assert_equal %Q{whoops({"body":null});\n}, get("/?url=/&callback=whoops").body
+    assert_equal %Q{feed({"body":#{pets_json}});}, get("/?url=#{pets_url}&callback=feed").body
+    assert_equal %Q{feed({"body":#{pets_json}});}, get("/?url=#{pets_url.sub('http://','')}&callback=feed").body
+    assert_equal %Q{yarrr_11({"body":#{pets_json}});}, get("/?url=#{pets_url}&callback=yarrr_11").body
+    assert_equal %Q{whoops({"body":null});}, get("/?url=/&callback=whoops").body
+  end
+
+  def test_array_with_one_url
+    url = 'http://elv1s.ru/x/pets.txt'
+    json = open(url).read.to_json
+    assert_equal %Q{grab_callback({"body":[#{json}]});}, get("/?urls[]=#{url}").body
+  end
+
+  def test_array_with_two_urls
+    urls = ['http://elv1s.local/x/pets.txt', 'http://elv1s.local/x/main.js']
+    json = urls.map {|u| open(u).read}.to_json
+    assert_equal %Q{grab_callback({"body":#{json}});}, get('/', {'urls'=>urls}).body
   end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
